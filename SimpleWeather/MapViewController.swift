33ae6d4aec032a2290575fffe4c45a19baadabb0
Antonio Ruffolo <antonioruffolo2@gmail.com>
2019-01-29 01:20:50 +0100

    first commit

---
+++ SimpleWeather/MapViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  SimpleWeather
+//
+//  Created by Antonio Ruffolo on 28/01/2019.
+//  Copyright © 2019 Antonio Ruffolo. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+class MapViewController: UIViewController
+{
+  @IBOutlet weak var mapView: MKMapView!
+  
+  override func viewDidLoad()
+  {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+}
